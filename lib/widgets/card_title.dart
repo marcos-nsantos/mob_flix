834b908865a4802ea5b933c6f4a1666767ec9813
Marcos Santos <marcos.diego.ns@outlook.com>
2023-07-25 18:53:52 -0500

    style: replace padding widget to container with margin of 20px in all sides

--- lib/widgets/card_title.dart
+++ lib/widgets/card_title.dart
@@ -8,8 +8,8 @@ class CardTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
+    return Container(
+      margin: const EdgeInsets.all(20.0),
       child: Column(
         children: [
           const Align(
