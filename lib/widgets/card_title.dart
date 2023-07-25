b0c7ee23dabfd9ffbe39fd2fe4ecc6c7efc660d2
Marcos Santos <marcosdiego.ns01@gmail.com>
2023-07-25 08:32:29 -0500

    feat: add category button on card title widget

--- lib/widgets/card_title.dart
+++ lib/widgets/card_title.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mob_flix/widgets/category_button.dart';
 
 class CardTitle extends StatelessWidget {
   final String title;
@@ -9,20 +10,29 @@ class CardTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Container(
-        height: 200.0,
-        width: 150.0,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0),
-          color: Colors.grey,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20.0),
-          child: Image.asset(
-            title,
-            fit: BoxFit.fill,
+      child: Column(
+        children: [
+          const Align(
+            alignment: AlignmentDirectional.topStart,
+            child: CategoryButton(name: 'Aventura', color: Colors.blue),
           ),
-        ),
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            height: 200.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.grey,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset(
+                title,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
